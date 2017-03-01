aa1f7403c8626958dd090a0dba7ee36a7ab27649
zhangtianle <18351936508@163.com>
2017-03-01 14:58:26 +0800

    sort

---
+++ 1.sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+//≤Â»Î≈≈–Ú 
+
+int main(void)
+{
+	int num[10] = {10,13,2,42,35,4,43,64,42,65};
+	int i = 0;
+	int t_i = 0;
+	int temp = 0;
+	
+	for(i = 1;i < 10;i ++)
+	{
+	    t_i = i;
+		do
+		{
+		    if(num[t_i - 1] > num[t_i])
+		    {
+			    temp = num[t_i - 1];
+				num[t_i - 1] = num[t_i];
+				num[t_i] = temp;	
+		    }
+			t_i --;	
+		}while((t_i >= 1));
+	} 
+	
+	for(i = 0;i < 10;i ++)
+	{
+	    printf("%d  ",num[i]);
+	} 
+	
+	return 0;
+} 
